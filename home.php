4f6923d9e1c30e45194ae52dfaafff5be22a3ea6
jllowen <322779@student.mboutrecht.nl>
2019-11-19 22:07:06 +0100

    Update home.php

--- home.php
+++ home.php
@@ -18,7 +18,9 @@
         <div class="jumbotron jumbotron-fluid">
   <div class="container">
     <h1 class="display-4">Fluid jumbotron</h1>
-    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    <p class="lead">In de regio Utrecht, op een steenworp afstand van de Jaarbeurs Utrecht,  vindt u conferentiecentrum Utrecht. 
+    Conferentiehotel Kontakt der Kontinenten ligt op slechts 10 minuten rijden van Utrecht in Soesterberg.
+     Met conferentiezalen, workshopruimtes, 131 hotelkamers en een keuken die gerechten met een wereldse twist serveert zijn wij een geslaagde locatie voor uw conferentie, congres en meerdaagse bijeenkomst.</p>
   </div>
 </div>
       </div>
