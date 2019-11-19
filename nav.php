17c3a4639ae66443e069ef5f2ba12c4157edefef
Jens Reimer <322494.am1c.blok2@gmail.com>
2019-11-19 20:23:25 +0100

    hutds

--- nav.php
+++ nav.php
@@ -2,10 +2,10 @@
 
 <div class="navbar">
   <a class="active" href="./index.php?content=home"><i class="fa fa-fw fa-home"></i> Home</a>
-  <a href="#"><i class="fa fa-fw fa-book"></i> Agenda</a>
-  <a href="#"><i class="fa fa-fw fa-info"></i> Info</a>
+  <a href="./index.php?content=agenda"><i class="fa fa-fw fa-book"></i> Agenda</a>
+  <a href="./index.php?content=info"><i class="fa fa-fw fa-info"></i> Info</a>
   <a href="./index.php?content=contact"><i class="fa fa-fw fa-map"></i> Contact</a>
-  <a href="#"><i class="fa fa-sign-in"></i> Inloggen</a>
-  <a href="#"><i class="fa fa-user-plus"></i> Registreren</a>
+  <a href="./index.php?content=loginform"><i class="fa fa-sign-in"></i> Inloggen</a>
+  <a href="./index.php?content=registerform"><i class="fa fa-user-plus"></i> Registreren</a>
   </div>
 
