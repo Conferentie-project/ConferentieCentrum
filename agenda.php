f97fde3a756f2840bebbe6eae211acda7ac88b00
Jens Reimer <322494.am1c.blok2@gmail.com>
2019-11-19 20:54:51 +0100

    hutds

--- agenda.php
+++ agenda.php
@@ -15,8 +15,30 @@
                             </div>
                             <div class="col-sm-8">
                                 <h1>Conferentie Watermanagement</h1><br>
-                                <h5>hutds</h5><br>
+                                <h5>info over evenement hier</h5><br>
                                 <a href="./index.php?content=watermanagement"><i class="fa fa-fw fa-book"></i>Ga naar evenement</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div>
+        <div class="row">
+            <div class="col-sm">
+                <div class="jumbotron jumbotron-fluid">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <p class="lead"><img src="./img/logo.jpg" href="./index.php?content=home" class="responsive"></p>
+                            </div>
+                            <div class="col-sm-8">
+                                <h1>*Ander evenement*</h1><br>
+                                <h5>info over evenement hier</h5><br>
+                                <a href="./index.php?content="><i class="fa fa-fw fa-book"></i>Ga naar evenement</a>
                             </div>
                         </div>
                     </div>
