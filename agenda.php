0d6b1780ac31b499a2d3c46a9ef90144f878d920
jllowen <322779@student.mboutrecht.nl>
2019-11-19 20:27:02 +0100

    Update nav.php

--- agenda.php
+++ agenda.php
@@ -1,3 +1,5 @@
+
+
 <h1 class="display-4"></h1>
 
 <main class="container">
--- nav.php
+++ nav.php
@@ -2,10 +2,15 @@
 
 <div class="navbar">
   <a class="active" href="./index.php?content=home"><i class="fa fa-fw fa-home"></i> Home</a>
-  <a href="#"><i class="fa fa-fw fa-book"></i> Agenda</a>
-  <a href="#"><i class="fa fa-fw fa-info"></i> Info</a>
+  <a href="./index.php?content=agenda"><i class="fa fa-fw fa-book"></i> Agenda</a>
+  <a href="./index.php?content=info"><i class="fa fa-fw fa-info"></i> Info</a>
   <a href="./index.php?content=contact"><i class="fa fa-fw fa-map"></i> Contact</a>
-  <a href="#"><i class="fa fa-sign-in"></i> Inloggen</a>
-  <a href="#"><i class="fa fa-user-plus"></i> Registreren</a>
+<<<<<<< HEAD
+  <a href="./index.php?content=sign-in"><i class="fa fa-sign-in"></i> Inloggen</a>
+  <a href="./index.php?content=registeren"><i class="fa fa-user-plus"></i> Registreren</a>
+=======
+  <a href="./index.php?content=loginform"><i class="fa fa-sign-in"></i> Inloggen</a>
+  <a href="./index.php?content=registerform"><i class="fa fa-user-plus"></i> Registreren</a>
+>>>>>>> 21193540e6d82ebbc25e66f0377972d408a7330b
   </div>
 
