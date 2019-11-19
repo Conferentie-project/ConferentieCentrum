f17a13fa93cff56d4ec5ceba6ef255114b63f972
jllowen <322779@student.mboutrecht.nl>
2019-11-19 21:47:20 +0100

    Update home.php

--- home.php
+++ home.php
@@ -12,8 +12,15 @@
             Op deze site vind je snel de meest bijzondere conferentielocatie. 
             Via de agenda pagina vind u de verschillende conferentcies die er plaats gaan vinden.
  </p>
+ 
           </div>
         </div>
+        <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Fluid jumbotron</h1>
+    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+  </div>
+</div>
       </div>
       <div class="col-sm">
       <p class="lead"><img src="./img/home.jpg" href="./index.php?content=home" class="responsive"></p>
