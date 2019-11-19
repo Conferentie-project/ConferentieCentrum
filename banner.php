c2acbee6f68b9c685c8324c205de8300651196b7
Alex van der Wel <a.vanderwel32@gmail.com>
2019-11-19 18:09:57 +0100

    Banner & Navbar
    Banner image gemaakt en navbar icons geregeld

--- banner.php
+++ banner.php
@@ -1,4 +1,2 @@
-<div class="jumbotron">
-    <h1 class="display-4">Foto hier</h1>
-    <p>Hier komt een foto en maybe wat tekst</p>
-</div>
+
+<img src="./img/banner.png"href="./index.php?content=home" class="responsive">
--- nav.php
+++ nav.php
@@ -1,5 +1,8 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+<
+
+
 <div class="navbar">
   <a class="active" href="#"><i class="fa fa-fw fa-home"></i> Home</a>
   <a href="#"><i class="fa fa-fw fa-book"></i> Agenda</a>
