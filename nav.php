ae49b80a85e2c6482c9c564e9372a71c7a50f5fd
Alex van der Wel <a.vanderwel32@gmail.com>
2019-11-19 17:38:55 +0100

    navbar

--- nav.php
+++ nav.php
@@ -1,9 +1,10 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <div class="navbar">
-  <a href="#" class="pull-left"><img src="./"></a> 
   <a class="active" href="#"><i class="fa fa-fw fa-home"></i> Home</a>
-  <a href="#"><i class="fa fa-fw fa-search"></i> Search</a>
-  <a href="#"><i class="fa fa-fw fa-envelope"></i> Contact</a>
-  <a href="#"><i class="fa fa-fw fa-user"></i> Login</a>
+  <a href="#"><i class="fa fa-fw fa-book"></i> Agenda</a>
+  <a href="#"><i class="fa fa-fw fa-info"></i> Info</a>
+  <a href="#"><i class="fa fa-fw fa-map"></i> Contact</a>
+  <a href="#"><i class="fa fa-sign-in"></i> Inloggen</a>
+  <a href="#"><i class="fa fa-user-plus"></i> Registreren</a>
 </div>
