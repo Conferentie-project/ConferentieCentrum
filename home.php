84f754f1083cae3ecf4ed6f090cb5c281c44c1c2
Alex van der Wel <a.vanderwel32@gmail.com>
2019-11-19 11:27:31 +0100

    Update css

--- home.php
+++ home.php
@@ -4,11 +4,22 @@
 
 <br>
 
-<div class="jumbotron jumbotron-fluid">
+<div>
+  <div class="row">
+    <div class="col-sm">
+    <div class="jumbotron jumbotron-fluid">
   <div class="container">
     <h1 class="display-4">Fluid jumbotron</h1>
     <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
   </div>
 </div>
+    </div>
+    <div class="col-sm">
+      IMAGES hiero a neef
+    </div>
+  </div>
+</div>
+
+
 
 </main>
