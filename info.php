75e361a22199deac2746861f7cc3c545c003527f
jllowen <322779@student.mboutrecht.nl>
2020-02-02 22:13:55 +0100

    w

--- info.php
+++ info.php
@@ -8,18 +8,16 @@
         <div class="jumbotron jumbotron-fluid">
           <div class="container">
             <h1 class="display-4">Het Conferentiecentrum </h1>
-            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce enim dui, molestie et metus id, feugiat tempus metus.
-             Suspendisse a ultricies ipsum, sed faucibus elit. Nullam diam lectus, accumsan non sem sit amet, aliquam accumsan libero. Nulla interdum ante quis enim vulputate molestie. Integer neque massa, tristique dictum lacus eget, porttitor sodales magna. Ut quis nunc massa. Suspendisse iaculis sapien vitae efficitur consequat. Suspendisse mollis ex nec felis consectetur, in rhoncus enim consectetur. Aliquam facilisis dui vitae purus porta,
-             et commodo felis lobortis. Nulla posuere id turpis a placerat. Aliquam lacinia felis elit, ut faucibus urna bibendum nec.
+            <p class="lead">Bij onze Conferentiecentrum gaat het om een vergadering of een bijeenkomst van meerdere mensen (meestal van eenzelfde organisatie) die met elkaar spreken en/of afspraken maken over de gemeenschappelijke toekomst. Tijdens een conferentie komen tientallen sprekers aan het woord, die openlijk discussiëren om verschillende standpunten te verdedigen.
+Een conferentie heeft veel overeenkomsten met een congres, waarbij een conferentie vaak een maatschappelijk of sociaal thema heeft, gaat het bij een congres om een zakelijk of wetenschappelijk thema.
             </p>
           </div>
         </div>
         <div class="jumbotron jumbotron-fluid">
   <div class="container">
     <h1 class="display-4">Informatie over de conferentie</h1>
-    <p class="lead">Quisque consequat nisi ante, sit amet accumsan nisi gravida vitae. Sed auctor pellentesque mollis. Donec iaculis egestas nunc, maximus vestibulum orci. Fusce ut fermentum sapien. Nulla molestie hendrerit diam sed porta. Vestibulum vehicula leo diam, non malesuada ligula fringilla euismod.
-     Integer suscipit quam dictum libero tincidunt, at tincidunt nisl dictum. Nullam eu tincidunt nunc. Praesent sollicitudin in odio vel eleifend. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-     Cras egestas lacinia enim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer sit amet ornare eros, faucibus feugiat lectus. Curabitur tortor ligula, consectetur ac egestas id, malesuada ut nunc. Vivamus interdum commodo nisl eget fermentum. In id facilisis eros.</p>
+    <p class="lead">Op slechts 2 minuten van Utrecht centraal station ligt het Conferentiecentrum, een informele conferentielocatie dicht bij het station. Onze vergaderzalen zijn flexibel aan te passen op groepen van verschillende grootte en zijn allemaal gelijkvloers. Voor een pauze, of een subsessie in groepen, kunt u de terrasdeuren openslaan en gebruik maken van het terras wat bij de meeste zalen aanwezig is. Als conferentiecentrum hebben we ook een aanbod teambuildingsactiviteiten.
+De centrale ligging in Midden-Nederland, en de ruime (gratis) parkeermogelijkheid maakt de Jaarbeurs tot een ideale locatie voor uw conferentie. Ons uitstekende horecateam regelt voor u catering op maat. Voor wie na afloop nog even wil blijven is er een prachtige Lounge, zonnig terras en groot Grand Café waar nog tot in de late uurtjes kan worden nagepraat.</p>
   </div>
 </div>
       </div>
